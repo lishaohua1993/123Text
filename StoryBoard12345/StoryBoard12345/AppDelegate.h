1f51b5a4707358a637c3e1c78c73ea617c18893f
laouhn lsh <981104034@qq.com>
2016-01-30 10:27:57 +0800

    dd

---
+++ StoryBoard12345/StoryBoard12345/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoryBoard12345
+//
+//  Created by laouhn lsh on 16/1/27.
+//  Copyright © 2016年 lsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoryBoard12345/StoryBoard12345/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoryBoard12345
+//
+//  Created by laouhn lsh on 16/1/27.
+//  Copyright © 2016年 lsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
